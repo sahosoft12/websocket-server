3fcf80fa61ed33b47ebed3aac8e636e0e3a5b801
sahosoft12 <132063023+sahosoft12@users.noreply.github.com>
2023-09-22 16:22:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const app = require('express')();
-const httpServer = require('http').createServer(app);
-const io = require('socket.io')(httpServer, {
-  cors: {origin : '*'}
+const app = require("express")();
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer, {
+  cors: { origin: "*" },
 });
 
 const port = 3000;
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-  socket.on('message', (message) => {
+  socket.on("message", (message) => {
     console.log(message);
-    io.emit('message', `${socket.id.substr(0, 2)}: ${message}`);
+
+    // message.id = socket.id;
+    // io.emit('message', `${socket.id.substr(0, 2)}: ${message}`);
+    io.emit("message", message);
   });
 
-  socket.on('disconnect', () => {
-    console.log('a user disconnected!');
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
   });
 });
 
